cfd455cc5d7c6e9736345fe70c7ed270fc852d08
Khumoyun <humoyunortiqov05@gmail.com>
2024-09-12 11:13:54 +0500

    models uyga vazifasi shu yerda yaratildi

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Book  # Modelingizni import qiling
+#
+# # Book modelini admin panelga qo'shish
+# admin.site.register(Book)
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    # Admin panelda ko'rsatiladigan maydonlar
+    list_display = ('title', 'author', 'published_date', 'isbn', 'pages', 'language')
+    # Qidirish maydoni
+    search_fields = ('title', 'author', 'isbn')
+
+
+# Modelni admin panelga ro'yxatdan o'tkazish
+admin.site.register(Book, BookAdmin)
--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    # Kitob nomi (Matnli maydon, maksimal uzunligi 200 ta belgi)
+    title = models.CharField(max_length=200)
+
+    # Muallif ismi (Matnli maydon, maksimal uzunligi 100 ta belgi)
+    author = models.CharField(max_length=100)
+
+    # Nashr etilgan sana (Sana maydoni)
+    published_date = models.DateField()
+
+    # ISBN raqami (Matnli maydon, maksimal uzunligi 13 ta belgi, yagona bo'lishi kerak)
+    isbn = models.CharField(max_length=13, unique=True)
+
+    # Sahifalar soni (Butun sonli maydon)
+    pages = models.IntegerField()
+
+    # Kitob tili (Matnli maydon, maksimal uzunligi 30 ta belgi)
+    language = models.CharField(max_length=30)
+
+    # Kitob janri (Matnli maydon, maksimal uzunligi 50 ta belgi)
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title  # Bu kitob nomini ko'rsatadi
+
