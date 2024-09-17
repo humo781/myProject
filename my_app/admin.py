b640f63c74037c410eab921eb000f94c687d276b
Khumoyun <humoyunortiqov05@gmail.com>
2024-09-17 07:22:50 +0500

    Vazifani shu repoda yaratdim keyinchalik boshqa repoda yarataman

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,18 +1,46 @@
-# from django.contrib import admin
-# from .models import Book  # Modelingizni import qiling
+# # from django.contrib import admin
+# # from .models import Book  # Modelingizni import qiling
+# #
+# # # Book modelini admin panelga qo'shish
+# # admin.site.register(Book)
 #
-# # Book modelini admin panelga qo'shish
-# admin.site.register(Book)
+# from django.contrib import admin
+# from .models import Book
+#
+# class BookAdmin(admin.ModelAdmin):
+#     # Admin panelda ko'rsatiladigan maydonlar
+#     list_display = ('title', 'author', 'published_date', 'isbn', 'pages', 'language')
+#     # Qidirish maydoni
+#     search_fields = ('title', 'author', 'isbn')
+#
+#
+# # Modelni admin panelga ro'yxatdan o'tkazish
+# admin.site.register(Book, BookAdmin)
+
 
 from django.contrib import admin
-from .models import Book
+from .models import Faculty, Group, Kafedra, Student, Subject, Teacher
 
-class BookAdmin(admin.ModelAdmin):
-    # Admin panelda ko'rsatiladigan maydonlar
-    list_display = ('title', 'author', 'published_date', 'isbn', 'pages', 'language')
-    # Qidirish maydoni
-    search_fields = ('title', 'author', 'isbn')
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'faculty')
 
-# Modelni admin panelga ro'yxatdan o'tkazish
-admin.site.register(Book, BookAdmin)
+@admin.register(Kafedra)
+class KafedraAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'kafedra', 'group')
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'kafedra')
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'subject')
--- my_app/models.py
+++ my_app/models.py
@@ -1,28 +1,72 @@
+# from django.db import models
+#
+#
+# class Book(models.Model):
+#     # Kitob nomi (Matnli maydon, maksimal uzunligi 200 ta belgi)
+#     title = models.CharField(max_length=200)
+#
+#     # Muallif ismi (Matnli maydon, maksimal uzunligi 100 ta belgi)
+#     author = models.CharField(max_length=100)
+#
+#     # Nashr etilgan sana (Sana maydoni)
+#     published_date = models.DateField()
+#
+#     # ISBN raqami (Matnli maydon, maksimal uzunligi 13 ta belgi, yagona bo'lishi kerak)
+#     isbn = models.CharField(max_length=13, unique=True)
+#
+#     # Sahifalar soni (Butun sonli maydon)
+#     pages = models.IntegerField()
+#
+#     # Kitob tili (Matnli maydon, maksimal uzunligi 30 ta belgi)
+#     language = models.CharField(max_length=30)
+#
+#     # Kitob janri (Matnli maydon, maksimal uzunligi 50 ta belgi)
+#     genre = models.CharField(max_length=50)
+#
+#     def __str__(self):
+#         return self.title  # Bu kitob nomini ko'rsatadi
+
 from django.db import models
 
-
-class Book(models.Model):
-    # Kitob nomi (Matnli maydon, maksimal uzunligi 200 ta belgi)
-    title = models.CharField(max_length=200)
-
-    # Muallif ismi (Matnli maydon, maksimal uzunligi 100 ta belgi)
-    author = models.CharField(max_length=100)
-
-    # Nashr etilgan sana (Sana maydoni)
-    published_date = models.DateField()
-
-    # ISBN raqami (Matnli maydon, maksimal uzunligi 13 ta belgi, yagona bo'lishi kerak)
-    isbn = models.CharField(max_length=13, unique=True)
-
-    # Sahifalar soni (Butun sonli maydon)
-    pages = models.IntegerField()
-
-    # Kitob tili (Matnli maydon, maksimal uzunligi 30 ta belgi)
-    language = models.CharField(max_length=30)
-
-    # Kitob janri (Matnli maydon, maksimal uzunligi 50 ta belgi)
-    genre = models.CharField(max_length=50)
+class Faculty(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.title  # Bu kitob nomini ko'rsatadi
+        return self.name
 
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Kafedra(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    kafedra = models.ForeignKey(Kafedra, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Subject(models.Model):
+    name = models.CharField(max_length=255)
+    kafedra = models.ForeignKey(Kafedra, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
